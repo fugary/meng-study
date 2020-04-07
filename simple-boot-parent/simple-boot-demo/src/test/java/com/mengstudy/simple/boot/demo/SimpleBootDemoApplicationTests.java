4d1843e24e2280b62c599336daeb03dbcb208fbc
Gary Fu <xuebaiyouling@gmail.com>
2020-04-07 18:53:28 +0800

    simple spring boot project

---
+++ simple-boot-parent/simple-boot-demo/src/test/java/com/mengstudy/simple/boot/demo/SimpleBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mengstudy.simple.boot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
