05cf18a596092b9a40eaf9677df1c23ad6d9b09d
Gary Fu <xuebaiyouling@gmail.com>
2020-04-07 16:34:16 +0800

    simple spring boot project

---
+++ simple-boot-parent/simple-boot-transaction-parent/simple-boot-tx-db/src/main/java/com/mengstudy/simple/tx/db/SimpleBootTxDbApplication.java
@@ -0,0 +1,13 @@
+package com.mengstudy.simple.tx.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBootTxDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBootTxDbApplication.class, args);
+    }
+
+}
---
+++ simple-boot-parent/simple-boot-transaction-parent/simple-boot-tx-db/src/test/java/com/mengstudy/simple/tx/db/SimpleBootTxDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mengstudy.simple.tx.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBootTxDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
