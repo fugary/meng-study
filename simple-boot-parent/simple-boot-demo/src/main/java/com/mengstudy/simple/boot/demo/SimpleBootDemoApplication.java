985504666dec2c33e33bbb89160680275d6064ee
Gary Fu <xuebaiyouling@gmail.com>
2020-04-08 20:41:45 +0800

    SpringBoot项目日志配置

--- simple-boot-parent/simple-boot-demo/src/main/java/com/mengstudy/simple/boot/demo/SimpleBootDemoApplication.java
+++ simple-boot-parent/simple-boot-demo/src/main/java/com/mengstudy/simple/boot/demo/SimpleBootDemoApplication.java
@@ -6,7 +6,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,12 +23,12 @@ public class SimpleBootDemoApplication {
 		SpringApplication.run(SimpleBootDemoApplication.class, args);
 	}
 
-	@RequestMapping("/hello")
+	@GetMapping("/hello")
 	public String hello(){
 		return "Hello World!";
 	}
 
-	@RequestMapping("/users")
+	@GetMapping("/users")
 	public List<User> users(){
 		return userMapper.selectList(null);
 	}
